04fe7d9d2ce6578c710fa8c57c7b509b16b4f580
“test” <test@163.com>
2015-09-30 11:33:36 +0800

    init project files

---
+++ client/app/app.router.js
@@ -0,0 +1,13 @@
+﻿angular.module('console.imgTag')
+    .config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise('/index');
+        $stateProvider.state("index",{
+            url:'/index',
+            templateUrl:'/client/views/index.html'
+        })
+    }])
+    .run(['$rootScope', '$state', '$stateParams', function ($rootScope, $state, $stateParams) {
+        $rootScope.$state = $state;
+        $rootScope.$stateParams = $stateParams;
+
+    }]);
